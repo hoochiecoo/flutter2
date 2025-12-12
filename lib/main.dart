b9ac80e73fd3dc4fa80baf9ed533e6447b3718c7
Anatoly <waveinspectro@gmail.com>
2025-12-12 11:16:03 +0100

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -18,26 +18,19 @@ class WebViewApp extends StatefulWidget {
 class _WebViewAppState extends State<WebViewApp> {
   late final WebViewController controller;
   
-  // Целевая ссылка
   final String targetUrl = 'https://bowlmates.club/user/UI/index-tw-pwa2.html?page=twHome';
 
   @override
   void initState() {
     super.initState();
-    
-    // Инициализация контроллера WebView
     controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setBackgroundColor(const Color(0x00000000))
       ..setNavigationDelegate(
         NavigationDelegate(
           onProgress: (int progress) {
-            // Можно добавить индикатор загрузки
             debugPrint('WebView is loading (progress : $progress%)');
           },
-          onPageStarted: (String url) {},
-          onPageFinished: (String url) {},
-          onWebResourceError: (WebResourceError error) {},
         ),
       )
       ..loadRequest(Uri.parse(targetUrl));
@@ -46,7 +39,6 @@ class _WebViewAppState extends State<WebViewApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // SafeArea важен, чтобы контент не залезал под "челку" и статус бар
       body: SafeArea(
         child: WebViewWidget(controller: controller),
       ),
